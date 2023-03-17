0aec7a86388e9400a7cb1a6869a5a082777a6c40
noracato <ncschinkel@gmail.com>
2023-03-17 14:21:32 +0100

    Parse the ETM url from the config_dict and create seperate yml based methods

--- src/etm_service/config.py
+++ src/etm_service/config.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 class Config:
     CONFIG_PATH = Path(__file__).parents[2].resolve() / 'config' / 'config.yml'
+    FROM_CONF_PATH = True
 
     class __Config:
         def __init__(self, data):
@@ -21,7 +22,13 @@ class Config:
         '''Get main keys of the config as attributes'''
         return self.instance.data.get(key, None)
 
+    def __setattr__(self, key, value):
+        self.instance.data[key] = value
+
     def _load(self):
+        if not self.FROM_CONF_PATH:
+            return {}
+
         with open(self.CONFIG_PATH, 'r') as f:
             doc = yaml.load(f, Loader=yaml.FullLoader)
 
