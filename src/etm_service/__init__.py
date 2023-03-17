d92de6d6a5c570758a1557f0b64092c306331131
noracato <ncschinkel@gmail.com>
2023-03-17 14:21:32 +0100

    Parse the ETM url from the config_dict and create seperate yml based methods

--- src/etm_service/__init__.py
+++ src/etm_service/__init__.py
@@ -26,28 +26,45 @@ def retrieve_results_and_write():
     data_requests.write_to(destination)
 
 
-def retrieve_results(scenario_id, from_dict={}, config_path=CONFIG_PATH, config_name='etm_service') -> dict:
+def retrieve_results(scenario_id: int, config_dict: dict) -> dict:
     '''
-    Retrieves the ETM outputs as specified in either the from_dict or the config,
+    Retrieves the ETM outputs as specified under the config_dict's 'config' key,
     and returns the results in a dict. Sets the correct CONFIG path for retrieving main
     config like the ETM api url.
 
     Params:
         scenario_id (int):  ETM scenario ID to communicate with;
-        from_dict (dict):   Data requests in dict format;
+        from_dict (dict):   Data requests in dict format; Contains at least the
+                            following keys: 'api_url' (url of ETM) and 'config'
+                            (a dict of data requests)
+    '''
+    # Update configs
+    Config.FROM_CONF_PATH = False
+    Config().api_url = config_dict['api_url']
+
+    # Create requests
+    data_requests = DataRequests.from_dict(config_dict['config'])
+
+    return retrieve_from_requests(scenario_id, data_requests)
+
+
+def retrieve_results_from_ymls(scenario_id, config_path=CONFIG_PATH,
+    config_name='etm_service') -> dict:
+    '''
+    Retrieves the ETM outputs as specified in config ymls, and returns the results in a dict.
+    Sets the correct CONFIG path for retrieving main config like the ETM api url.
+
+    Params:
+        scenario_id (int):  ETM scenario ID to communicate with;
         config_path (Path): Path to the folder containing the config file(s);
-        config_name (str):  If from_dict was not specified, this yml file's name
-                            will be checked out in the config folder to be loaded
-                            into data requests.
+        config_name (str):  This yml file's name will be checked out in the config folder to
+                            be loaded into data requests.
     '''
     # Update configs (contains etm api url)
     Config.CONFIG_PATH = config_path / 'config.yml'
 
     # Create requests
-    if not from_dict:
-        data_requests = DataRequests.load_from_path(config_path / f'{config_name}.yml')
-    else:
-        data_requests = DataRequests.from_dict(from_dict)
+    data_requests = DataRequests.load_from_path(config_path / f'{config_name}.yml')
 
     return retrieve_from_requests(scenario_id, data_requests)
 
@@ -64,34 +81,54 @@ def retrieve_from_requests(scenario_id: int, data_requests: DataRequests):
     return data_requests.to_dict()
 
 
-def scale_copy_and_send(scenario_id, holon_outcomes, from_dict={}, config_path=CONFIG_PATH, config_name='scaling_factors') -> int:
+def scale_copy_and_send(scenario_id: int, holon_outcomes: dict, config_dict: dict) -> int:
     '''
     Scales and updates sliders in the ETM, returns the ETM scenario ID of the copied and
     set scenario.
 
     Params:
         scenario_id (int):      ETM scenario ID to communicate with;
-        holon_outcomes (dict):  Holon outcomes in json or dict format
-        from_dict (dict):       Data requests in dict format;
-        config_path (Path):     Path to the folder containing the config file(s);
-        config_name (str):      If from_dict was not specified, this yml file's name
-                                will be checked out in the config folder to be loaded
-                                into data requests.
+        holon_outcomes (dict):  Holon outcomes in json or dict format; The keys should match
+                                the keys of the data requests
+        from_dict (dict):       Data requests in dict format; Contains at least the
+                                following keys: 'api_url' (url of ETM) and 'config'
+                                (a dict of data requests)
+
     '''
 
     # Update configs
+    Config.FROM_CONF_PATH = False
+    Config().api_url = config_dict['api_url']
+
+    data_requests = DataRequests.from_dict(config_dict['config'], action='SET')
+
+    return scale_copy_and_send_from_requests(scenario_id, holon_outcomes, data_requests)
+
+
+def scale_copy_and_send_from_ymls(scenario_id, holon_outcomes, config_path=CONFIG_PATH,
+    config_name='scaling_factors') -> int:
+    '''
+    Scales and updates sliders in the ETM, returns the ETM scenario ID of the copied and
+    set scenario. Based on Config ymls.
+
+    Params:
+        scenario_id (int):      ETM scenario ID to communicate with;
+        holon_outcomes (dict):  Holon outcomes in json or dict format
+        config_path (Path):     Path to the folder containing the config file(s);
+        config_name (str):      This yml file's name will be checked out in the config folder
+                                to be loaded into data requests.
+    '''
+    # Update configs
     Config.CONFIG_PATH = config_path / 'config.yml'
 
-    # Create requests
-    if not from_dict:
-        data_requests = DataRequests.load_from_path(config_path / f'{config_name}.yml', action='SET')
-    else:
-        data_requests = DataRequests.from_dict(from_dict, action='SET')
+    data_requests = DataRequests.load_from_path(config_path / f'{config_name}.yml', action='SET')
 
     return scale_copy_and_send_from_requests(scenario_id, holon_outcomes, data_requests)
 
 
 def scale_copy_and_send_from_requests(scenario_id, holon_outcomes, data_requests: DataRequests):
+    '''Called only by the other two scale_copy_and_send methods'''
+
     # Combine requests with HOLON outcomes
     combiner = Combiner(holon_outcomes)
     data_requests.combine(combiner)
