7d475802b8439912bb97201c07d42f42bb150259
Erik van Velzen <erik@evanv.nl>
2023-10-24 10:25:35 +0200

    More specific connection error

--- src/etm_service/etm_session/copy.py
+++ src/etm_service/etm_session/copy.py
@@ -3,15 +3,16 @@ import requests
 from etm_service.config import Config
 from .session import ETMSession
 
+
 class ETMCopySession(ETMSession):
-    '''Copies the ETM scenario and sets the new ID in the Config'''
+    """Copies the ETM scenario and sets the new ID in the Config"""
     ENDPOINT = '/'
 
     def send_request(self, _):
         payload = {'scenario': {'scenario_id': self.scenario_id}}
         yield self._handle_response(requests.post(Config().api_url, json=payload))
 
-    def _handle_response(self, response):
+    def _handle_response(self, response: requests.Response):
         if response.ok:
             return response.json()['id']
 
